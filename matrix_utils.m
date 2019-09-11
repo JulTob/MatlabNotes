0a3cb1c5469e76f515fe48ea531ec39b2f359c09
Julio <JulioToboso@gmail.com>
2019-09-11 12:25:07 +0200

    Create matrix_utils.m

---
+++ matrix_utils.m
@@ -0,0 +1,18 @@
+x = rand(5)
+
+element = x(3,1)
+fila = x(2,:)
+last = x(end,2);
+partial = x( 1:3,:)
+
+from_vect = fila(2)
+
+xMax = max(x)
+
+xRound = round(x)
+
+xSqrt = sqrt(x)
+
+element_mult = rand(5) .* rand(5)
+
+matrix_cross = rand(5) * eye(5)
