13867e936e24f1556828dc90871520c07be41e11
Julio <JulioToboso@gmail.com>
2020-06-17 20:27:47 +0200

    Create Cells_script.m

---
+++ Cells_script.m
@@ -0,0 +1,16 @@
+%% Cells_script
+% {}
+
+page{1}='Waba laba dub dub';
+page{2}='Pickle rick!';
+
+%% printit
+fprintf('\n');
+
+for ii= 1:length(page)
+    
+    fprintf('%s\n',page{ii});
+    
+ end
+
+fprintf('\n');
