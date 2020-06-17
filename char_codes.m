0cf6686ca584ae230e1d46ce96f5054e613126ec
Julio <JulioToboso@gmail.com>
2020-06-17 20:27:16 +0200

    Create char_codes.m

---
+++ char_codes.m
@@ -0,0 +1,11 @@
+function char_codes
+%ASCII Characters to check
+for ii=33:126
+    fprintf('%s',ii); % %s pide char
+                %char(ii)
+end
+%fprintf('\n');
+    fprintf('%s',10); % new line
+
+
+end
